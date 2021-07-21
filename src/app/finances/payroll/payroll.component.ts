0458bbcc9434ae4a1142985bc38ff616cfff1e56
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-07-21 09:55:08 -0400

    Add functionality to buttons

--- src/app/finances/payroll/payroll.component.ts
+++ src/app/finances/payroll/payroll.component.ts
@@ -1,21 +1,40 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
+import { ItemComponent } from '../item/item.component';
 
 @Component({
   selector: 'fin-payroll',
   templateUrl: './payroll.component.html',
   styleUrls: ['./payroll.component.css']
 })
-export class PayrollComponent implements OnInit, AfterViewInit {
+export class PayrollComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  @ViewChild('item', {read: ViewContainerRef}) item;
+
+  constructor(private authService: AuthService,
+              private injector: Injector,
+              private cfr: ComponentFactoryResolver) { }
 
   ngOnInit(): void {}
   
-  ngAfterViewInit(): void {}
-
   logout() {
     this.authService.logout();
+  }
+
+  addItemComponent() {
+    const cmpFactory = this.cfr.resolveComponentFactory(ItemComponent);
+    const componentRef = cmpFactory.create(this.injector);
+    this.item.insert(componentRef.hostView);
+  }
+
+  getData() {
+    let items: any[] = [];
+    const data = document.querySelectorAll('fin-item');
+    data.forEach(item => {
+      const key = item.children[0][0].value.toLowerCase();
+      const amount = parseInt((item.children[0][1].value).match(/\d+/g).join(''));
+      items.push({ [key]: amount });
+    });    
   }
 
 }
