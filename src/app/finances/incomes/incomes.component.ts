317317fe173176816b5d29cdb0d9f35a32b8de8d
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-07-30 20:24:56 -0400

    Update add income box

--- src/app/finances/incomes/incomes.component.ts
+++ src/app/finances/incomes/incomes.component.ts
@@ -10,8 +10,6 @@ import { ItemComponent } from '../item/item.component';
 })
 export class IncomesComponent implements OnInit {
 
-  @ViewChild('item', {read: ViewContainerRef}) item;
-
   constructor(private authService: AuthService,
               private injector: Injector,
               private cfr: ComponentFactoryResolver,
@@ -23,20 +21,8 @@ export class IncomesComponent implements OnInit {
     this.authService.logout();
   }
 
-  addItemComponent() {
-    const cmpFactory = this.cfr.resolveComponentFactory(ItemComponent);
-    const componentRef = cmpFactory.create(this.injector);
-    this.item.insert(componentRef.hostView);
-  }
-
-  getData() {
-    let items: any[] = [];
-    const data = document.querySelectorAll('fin-item');
-    data.forEach(item => {
-      const key = item.children[0][0].value.toLowerCase();
-      const amount = parseInt((item.children[0][1].value).match(/\d+/g).join(''));
-      items.push({ [key]: amount });
-    });    
+  addIncome() {
+    
   }
 
   goToExpenses() {
@@ -44,7 +30,6 @@ export class IncomesComponent implements OnInit {
   }
 
   submitPage() {
-    this.getData();
     this.goToExpenses();
   }
 
