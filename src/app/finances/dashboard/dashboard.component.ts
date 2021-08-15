bf64c489d1e2bd15f7e630b196f925c24d98afa2
Julio Cid <59150903+Julius-cell@users.noreply.github.com>
2021-08-15 14:05:19 -0400

    Add functionalities to dashboard

--- src/app/finances/dashboard/dashboard.component.ts
+++ src/app/finances/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'fin-dashboard',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public incomes: any[] = [];
+  public totalIncome: number = 0;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.getIncomes();
+  }
+
+  getIncomes() {
+    this.authService.getUserInfo().subscribe(res => {
+      const incomes = res.accounts[0].incomes;
+      this.sumIncomesAmounts(incomes);
+    });
+  }
+
+  getExpenses() {
+    this.authService.getUserInfo().subscribe(res => {
+      const expenses = res.accounts[0].expenses;
+    });
+  }
+
+  sumIncomesAmounts(incomes) {
+    const amounts = incomes.map(income => Object.values(income));
+    this.totalIncome = amounts.reduce((acc, val) => parseInt(acc) + parseInt(val));
   }
 
 }
